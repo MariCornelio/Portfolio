c98b3a048d3f9f065d42abbe8a7be81f90f7d6d2
Mari Cornelio <katherinacornelio@gmail.com>
2023-06-21 15:29:15 -0500

    Adding animations in word Component - adding scroll on hover - adding modal window - adding modal window lock interaction

--- src/app/pages/works/works.component.ts
+++ src/app/pages/works/works.component.ts
@@ -7,8 +7,25 @@ import AOS from 'aos';
   styleUrls: ['./works.component.css'],
 })
 export class WorksComponent implements OnInit {
+  constructor() {}
+  // @ViewChild('icon') icon!: ElementRef;
+  // iconClass!: string;
+  valid: boolean = false;
+
   ngOnInit(): void {
     AOS.init();
     window.addEventListener('load', AOS.refresh);
+  }
+
+  onClick() {
+    this.valid = true;
+  }
+  close() {
+    // if (this.icon.nativeElement.className === 'Icon') {
+    //   this.iconClass = 'Icon open';
+    // } else {
+    //   this.iconClass = 'Icon';
+    // }
+    this.valid = false;
   }
 }
