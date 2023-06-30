2b6b9d1085896524f54d16e6cfcceb70f027990e
Mari Cornelio <katherinacornelio@gmail.com>
2023-06-30 18:30:03 -0500

    Rendering footer   - Animating componentContact   - fixing footer   - Adding links in the footer that lead to the top of the page

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  constructor(private router: Router) {}
+  onClick(value: string) {
+    if (value === 'home') {
+      this.router.navigate(['']);
+      window.scrollTo({ top: 0 });
+    }
+    if (value === 'works') {
+      this.router.navigate(['works']);
+      window.scrollTo({ top: 0 });
+    }
+    if (value === 'contact') {
+      this.router.navigate(['contact']);
+      window.scrollTo({ top: 0 });
+    }
+  }
 }
