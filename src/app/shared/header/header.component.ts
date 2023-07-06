323fa6179292773faa2e0676ee1ea652743efd25
Mari Cornelio <katherinacornelio@gmail.com>
2023-07-06 12:56:36 -0500

    Fixing the hamburger button functionality   - Using mediaqueries for mobile version   - Adding social networks to the navigation menu   - Adding event emission to the footer component to close the hamburger button.   - Fixing hamburger button with fixed   - Correcting measurements

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../service/modal.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import { ModalService } from '../service/modal.service';
 })
 export class HeaderComponent implements OnInit {
   isCkecked: boolean = false;
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private router: Router) {}
   ngOnInit(): void {
     this.modalService.$checkedHamburger.subscribe(
       (value) => (this.isCkecked = value)
@@ -19,6 +20,20 @@ export class HeaderComponent implements OnInit {
       this.modalService.$checkedHamburger.emit(false);
     } else {
       this.modalService.$checkedHamburger.emit(true);
+    }
+  }
+  onClickRouter(value: string) {
+    if (value === 'home') {
+      this.router.navigate(['']);
+      window.scrollTo({ top: 0 });
+    }
+    if (value === 'works') {
+      this.router.navigate(['works']);
+      window.scrollTo({ top: 0 });
+    }
+    if (value === 'contact') {
+      this.router.navigate(['contact']);
+      window.scrollTo({ top: 0 });
     }
   }
 }
